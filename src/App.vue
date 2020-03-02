b5bf29459b386d2303e728d384af3f8471a281fa
ej.santoemma <ej.santoemma@trampolier.com>
2020-03-02 16:33:55 +0100

    --

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="float: left; margin-left: 100px" :class="maybe_disabled">
+      <input style="float: left; margin-right: 5px; max-width: 50px" ref="inp" id="inp" @input="f" v-model="quiz_number"/>
+      <span style="float: left; margin-right: 30px;" v-if="current_quiz">{{quiz[current_quiz].type}}</span>
+      <div style="float: left" v-show="current_quiz !== undefined">
+        <label for="a">A</label>
+        <input style="; margin-right: 10px" type="radio" id="a" value="a" v-model="answer" />
+        <label for="b">B</label>
+        <input style="; margin-right: 10px" type="radio" id="b" value="b" v-model="answer" />
+        <label for="c">C</label>
+        <input style="; margin-right: 10px" type="radio" id="c" value="c" v-model="answer" />
+        <label for="d">D</label>
+        <input style="; margin-right: 10px" type="radio" id="d" value="d" v-model="answer" />
+        <label for="e">E</label>
+        <input style="; margin-right: 30px" type="radio" id="e" value="e" v-model="answer" />
+
+        <span style="color: green;" v-if="current_quiz && answer === quiz[current_quiz].answer">OK</span>
+        <span style="color: red;" v-else-if="answer !== undefined">NO</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  watch: {
+    answer: function (current) {
+      if (this.current_quiz && current !== this.quiz[this.current_quiz].answer) {
+        this.maybe_disabled = 'disabled'
+        setTimeout(() => {
+          this.maybe_disabled = ''
+          this.$refs['inp'].focus()
+        }, 3000)
+      } else {
+        this.$refs['inp'].focus()
+      }
+    }
+  },
+  data() {
+    return {
+      quiz: {
+        1601: { type: "", answer: "e" },
+        1602: { type: "", answer: "a" },
+        1603: { type: "", answer: "c" },
+        1604: { type: "", answer: "a" },
+        1605: { type: "", answer: "b" },
+        1606: { type: "", answer: "e" },
+        1607: { type: "", answer: "c" },
+        1608: { type: "", answer: "d" },
+        1609: { type: "", answer: "e" },
+        1610: { type: "", answer: "b" },
+      },
+      quiz_number: undefined,
+      current_quiz: undefined,
+      answer: undefined,
+      maybe_disabled: '',
+    };
+  },
+  methods: {
+    f() {
+      this.current_quiz = undefined
+      this.answer = undefined
+
+      if (this.quiz_number in this.quiz) {
+        this.current_quiz = this.quiz_number
+      } 
+    },
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +80,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.disabled {
+    pointer-events: none;
+    opacity: 0.6;
 }
 </style>
